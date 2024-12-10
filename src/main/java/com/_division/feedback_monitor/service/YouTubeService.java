21915e9e967bcc98adebea64ef2b3918f92ebf49
Eojin <raeojin@gmail.com>
2024-12-10 15:18:21 +0900

    remove .env add api_Key info to .yml

--- src/main/java/com/_division/feedback_monitor/service/YouTubeService.java
+++ src/main/java/com/_division/feedback_monitor/service/YouTubeService.java
@@ -1,7 +1,10 @@
 package com._division.feedback_monitor.service;
 
+import com.fasterxml.jackson.core.JsonFactory;
 import com.google.api.services.youtube.YouTube;
 import org.springframework.stereotype.Service;
+
+import javax.imageio.spi.IIORegistry;
 
 @Service
 public class YouTubeService {
